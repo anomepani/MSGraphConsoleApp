95046f812c8cf2c8df50f059dc5772794701ccae
Arvind Mepani <arvindmepani@gmail.com>
2019-03-19 01:22:52 +0530

    Initial commit for configuration of MSGRaph using Console app.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSGraphConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graphClient=AuthenticationHelper.GetAuthenticatedClient();
+            var Users = graphClient.Users.Request().Top(400).GetAsync().Result;
+            _ = JsonConvert.SerializeObject(Users);
+
+            foreach (Microsoft.Graph.User item in Users)
+            {
+                Console.WriteLine(item.UserPrincipalName);
+
+            }
+            Console.WriteLine(Users.Count);
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
